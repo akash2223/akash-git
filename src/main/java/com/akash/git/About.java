ad7d8d42f9e93497e5ead2c37ba06949dd41d4dd
akash <kapil.shirke23@gmail.com>
2018-10-07 17:44:14 +0530

    first commit

---
+++ src/main/java/com/akash/git/About.java
@@ -0,0 +1,8 @@
+package com.akash.git;
+
+public class About {
+
+	public static void main(String[] args) {
+		System.out.println("About Info");
+	}
+}
---
+++ src/main/java/com/akash/git/JaiShreeRam.java
@@ -0,0 +1,8 @@
+package com.akash.git;
+
+public class JaiShreeRam {
+
+	public static void main(String[] args) {
+		System.out.println("Jai Shree Ram");
+	}
+}
